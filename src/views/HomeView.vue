20eb0b479ac8acfd22da58dfb3b3e54d571d679b
marocks31 <mollyarisley@gmail.com>
2022-04-20 12:44:04 -0500

    OAuth practice, intial commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,46 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
-</template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      albums: [],
+    };
+  },
+  created: function () {
+    this.albumsIndex();
+    this.newsIndex();
+    var code = this.$route.query.code;
+    if (code) {
+      axios.get("/auth/github/callback?code=" + code).then((response) => {
+        localStorage.setItem("github_access_token", response.data.access_token);
+        this.$router.push("/about");
+      });
+    }
+    console.log(this.$route.query.code);
+  },
+  methods: {
+    albumsIndex: function () {
+      axios.get("https://jsonplaceholder.typicode.com/albums").then((response) => (this.albums = response.data));
+    },
+    newsIndex: function () {
+      axios.get("/news_api").then((response) => {
+        console.log(response.data);
+      });
+    },
   },
 };
 </script>
+
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <a href="https://github.com/login/oauth/authorize?client_id=a87231a022a1a5793102">Sign into GitHub</a>
+    <div v-for="album in albums" v-bind:key="album.id">
+      <h2>{{ album.title }}</h2>
+    </div>
+  </div>
+</template>
+
+<style></style>
